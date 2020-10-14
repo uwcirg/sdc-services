8dbb998e68039815c4077d43574662662fa6fd6a
Ivan Cvitkovic <ivanc@uw.edu>
2020-10-14 13:05:31 -0700

    Add total attribute to FHIR bundle

--- sdc_services/models/r2/bundle.py
+++ sdc_services/models/r2/bundle.py
@@ -4,8 +4,9 @@ def as_bundle(fhir_resources, bundle_type='collection'):
         entries.append(fhir_resource)
 
     bundle_fhir = {
-        "resourceType": 'Bundle',
+        'resourceType': 'Bundle',
         'entry': entries,
         'type': bundle_type,
+        'total': len(entries),
     }
     return bundle_fhir
