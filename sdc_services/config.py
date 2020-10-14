68e4fc33ba8f95095d5d2172ff412a28c359a682
Ivan Cvitkovic <ivanc@uw.edu>
2020-10-14 12:54:37 -0700

    Add comment

--- sdc_services/config.py
+++ sdc_services/config.py
@@ -7,4 +7,5 @@ import os
 SERVER_NAME = os.getenv("SERVER_NAME")
 SECRET_KEY = os.getenv("SECRET_KEY")
 # URL scheme to use outside of request context
+# do not configure as https unless reverse proxy secured
 PREFERRED_URL_SCHEME = os.getenv("PREFERRED_URL_SCHEME", 'http')
