f6b168eba94ef090a951df8904495545b24ff39f
Ivan Cvitkovic <ivanc@uw.edu>
2020-10-21 18:19:58 -0700

    Add tests, example QNR with contained Questionnaire

--- tests/test_fhir.py
+++ tests/test_fhir.py
@@ -26,6 +26,17 @@ def test_extract_observation(empro_r2_questionnaire_response):
     assert obs['valueCoding'] == empro_r2_questionnaire_response['group']['question'][0]['answer'][1]['valueCoding']
 
 
+def test_extract_observation_contained_questionnaire(ironman_ss_r2_qnr_contained_questionnaire):
+    """Test Questionnaire contained in QuestionnaireResponse"""
+    qnr = QuestionnaireResponse.from_json(ironman_ss_r2_qnr_contained_questionnaire)
+    observations = qnr.extract()
+
+    assert observations[2]['code']['coding'][1]['code'] == 'general_pain'
+
+    assert qnr.questionnaire_res.item[3]['code'][0] == observations[3]['code']['coding'][1]
+    assert observations[3]['code']['coding'][1]['code'] == 'joint_pain'
+
+
 def test_extract_api(client, r2_questionnaire_response):
     result = client.post('/v/r2/fhir/$extract', json=r2_questionnaire_response)
     assert result.status_code == 200
